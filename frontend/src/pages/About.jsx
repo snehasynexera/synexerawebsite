a1c7870de10032b48b4596f7e9d9bd92a7719f38
aka12q <akansha.21SCSE1280025@galgotiasuniversity.edu.in>
2025-11-12 21:22:17 +0530

    Updated the Navbar

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,14 @@
+// src/pages/Home.jsx
+import { useEffect, useState } from "react";
+import { getHomeData } from "../api";
+import { Sparkle, HeartPulse, ChevronsRight } from "lucide-react";
+import Services from "./Services";
+
+export default function About() {
+
+  return (
+    <main className="min-h-screen bg-pink text-[#071234] text-center font-bold w-full overflow-x-hidden relative">
+        This is About
+    </main>
+  );
+}
